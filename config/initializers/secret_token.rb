8a04f7be3c28c2f1043b4f677b06c5b2b98afbdd
Gentry Sherrill <sherrill.gentry@gmail.com>
2012-03-24 18:19:17 -0400

    Base Rails Installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiAD::Application.config.secret_token = '8c2397939e5250abdea6feae07b1307f7e476e85b498db667aeb0b3f8c6d9ceced7337295af2e8b926eb15d96307371663b12dc097e80bb97ad84b76d5f538df'

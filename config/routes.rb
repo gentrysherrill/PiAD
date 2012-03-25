e017b8ee15aad5c92b434d335064f0bb85aa316a
Gentry Sherrill <sherrill.gentry@gmail.com>
2012-03-24 23:49:20 -0400

    Numerous updates; base application chrome, generic html contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PiAD::Application.routes.draw do
+  get "home/index"
+
+  resources :contacts
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +53,8 @@ PiAD::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  
+  root :to => 'home#index', as: 'home'
 
   # See how all your routes lay out with "rake routes"
 
